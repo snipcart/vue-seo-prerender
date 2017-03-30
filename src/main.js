350757294be0f1f56ea7d16f1b5dfa0e278effde
Charles Ouellet <charles.ouellet@gmail.com>
2017-03-30 08:27:04 -0400

    Fixed product URLs

--- src/main.js
+++ src/main.js
@@ -3,14 +3,22 @@ import App from './App.vue'
 import router from './router'
 import { sync } from 'vuex-router-sync'
 import store from './store'
+import VueConfig from 'vue-config';
 
 sync(store, router)
+
+const configs = {
+  baseUrl: 'https://vue-snipcart.netlify.com/'
+};
+
+Vue.use(VueConfig, configs);
 
 const root = new Vue({
   store,
   router,
   render: h => h(App)
 })
+
 
 document.addEventListener('DOMContentLoaded', () => {
   root.$mount('#app');
